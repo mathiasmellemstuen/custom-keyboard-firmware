c24b44f807640ec65801d2cdd3e3d24e2e66ec91
Mathias Mellemstuen <mathias.mellemstuen@gmail.com>
2022-07-29 15:17:26 +0200

    Changed keymap to a more logical one

--- CustomNordicMathias/keymap.c
+++ CustomNordicMathias/keymap.c
@@ -2,10 +2,10 @@
 #include <quantum/keymap_extras/keymap_norwegian.h>
 
 enum sofle_layers {
-    /* _M_XYZ = Mac Os, _W_XYZ = Win/Linux */
     _QWERTY,
     _CUSTOM_LAYER,
-    _CUSTOM_LAYER_2
+    _CUSTOM_LAYER_2,
+    _CUSTOM_LAYER_3
 };
 
 enum custom_keycodes {
@@ -21,43 +21,34 @@ enum custom_keycodes {
     KC_DLINE
 };
 
-
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-/*
- * QWERTY
- * ,-----------------------------------------.                    ,-----------------------------------------.
- * |  `   |   1  |   2  |   3  |   4  |   5  |                    |   6  |   7  |   8  |   9  |   0  |  `   |
- * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * | ESC  |   Q  |   W  |   E  |   R  |   T  |                    |   Y  |   U  |   I  |   O  |   P  | Bspc |
- * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * | Tab  |   A  |   S  |   D  |   F  |   G  |-------.    ,-------|   H  |   J  |   K  |   L  |   ;  |  '   |
- * |------+------+------+------+------+------|       |    |       |------+------+------+------+------+------|
- * |LShift|   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   N  |   M  |   ,  |   .  |   /  |RShift|
- * `-----------------------------------------/       /     \      \-----------------------------------------'
- *            | LGUI | LAlt | LCTR |LOWER | /Enter  /       \Space \  |RAISE | RCTR | RAlt | RGUI |
- *            |      |      |      |      |/       /         \      \ |      |      |      |      |
- *            `----------------------------------'           '------''---------------------------'
- */
 
 [_QWERTY] = LAYOUT(
   KC_ESC,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,  NO_PLUS,
   KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,  NO_ARNG,
   SFT_T(KC_CAPS),   KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L, NO_OSTR,  NO_AE,
   KC_LCTL,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_MEDIA_PLAY_PAUSE,     KC_MUTE,KC_N,    KC_M, KC_COMM,  KC_DOT, NO_MINS,  NO_QUOT,
-                 NO_LABK,KC_LALT,KC_LEFT_GUI, KC_SPACE, MO(_CUSTOM_LAYER),      KC_ENTER,  KC_BACKSPACE, KC_ALGR, NO_BSLS, NO_DIAE
+                 NO_LABK,KC_LALT,KC_LEFT_GUI, KC_SPACE, LT(_CUSTOM_LAYER, KC_ENTER),      KC_ENTER,  KC_BACKSPACE, KC_ALGR, KC_DEL, NO_DIAE
 ),
   [_CUSTOM_LAYER] = LAYOUT(
-  XXXXXXX , KC_F1,  KC_F2,  KC_F3 , KC_F4, KC_F5,                     KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11,
-  XXXXXXX , XXXXXXX, KC_UP, XXXXXXX, DYN_REC_START1, XXXXXXX,                     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, DYN_MACRO_PLAY1, KC_F12,
-  MO(_CUSTOM_LAYER_2) , KC_LEFT,KC_DOWN, KC_RIGHT, XXXXXXX,  XXXXXXX,                     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-  XXXXXXX , XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_PIPE, NO_GRV,
+  XXXXXXX , XXXXXXX,  XXXXXXX,  XXXXXXX , XXXXXXX, XXXXXXX,                     XXXXXXX, NO_BSLS, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+  XXXXXXX , XXXXXXX, KC_UP, XXXXXXX, DYN_REC_START1, XXXXXXX,                     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, DYN_MACRO_PLAY1, XXXXXXX,
+  MO(_CUSTOM_LAYER_2) , KC_LEFT,KC_DOWN, KC_RIGHT, MO(_CUSTOM_LAYER_3),  XXXXXXX,                     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+  XXXXXXX , XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, NO_GRV,
                     _______, _______, _______, _______, _______,     _______, _______, _______, _______, _______
   ),
   [_CUSTOM_LAYER_2] = LAYOUT(
-  XXXXXXX , XXXXXXX,  XXXXXXX, XXXXXXX , XXXXXXX, XXXXXXX,                     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+  XXXXXXX , XXXXXXX,  XXXXXXX, XXXXXXX , XXXXXXX, XXXXXXX,                     XXXXXXX, NO_PIPE, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
   XXXXXXX , XXXXXXX, XXXXXXX, XXXXXXX,XXXXXXX,XXXXXXX,                     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
   XXXXXXX , XXXXXXX,XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX,                     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
   XXXXXXX , XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX, XXXXXXX,     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, NO_ACUT,
+                    _______, _______, _______, _______, _______,     _______, _______, _______, _______, _______
+  ),
+  [_CUSTOM_LAYER_3] = LAYOUT(
+  XXXXXXX , KC_F1,  KC_F2, KC_F3 , KC_F4, KC_F5,                     KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11,
+  XXXXXXX , XXXXXXX, XXXXXXX, XXXXXXX,XXXXXXX,XXXXXXX,                     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_F12,
+  XXXXXXX , XXXXXXX,XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX,                     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+  XXXXXXX , XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX, XXXXXXX,     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
                     _______, _______, _______, _______, _______,     _______, _______, _______, _______, _______
   )
 };
